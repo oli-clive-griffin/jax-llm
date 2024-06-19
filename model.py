b9bc738305f1c2b2c127e51aad858445573545ec
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2024-06-19 18:18:03 -0400

    wip

--- model.py
+++ model.py
@@ -3,38 +3,28 @@ from jax import tree_util
 from dataclasses import dataclass
 from jax import random, Array
 import jax.numpy as jnp
-from einops import einsum, rearrange
 import jax.nn
-from functools import partial
+
 
 @jax.jit
 def mha(residual_SDm: Array, w_qkv_HDm3Dh):
-    '''
+    """
     residual_SD: (seq_len, dim_model)
     w_qkv_HDm3Dh: (n_heads, d_model 3 * d_head)
 
     returns: out_HSDh: (n_heads, seq_len, d_head)
-    '''
-    # qkv_HSDhx3 = einsum(residual_SDm, w_qkv_HDm3Dh, "s dm, h dm dh_x3 -> h s dh_x3")
+    """
     qkv_HSDhx3 = residual_SDm @ w_qkv_HDm3Dh
-
     q_HSDh, k_HSDh, v_HSDh = jnp.split(qkv_HSDhx3, 3, axis=-1)
-
-    # qkt_HSqSk = einsum(q_HSDh, k_HSDh, "h sq dh, h sk dh -> h sq sk") # QK^T
-    # h sq dh @ h dh sk -> h sq sk
     qkt_HSqSk = q_HSDh @ jnp.matrix_transpose(k_HSDh)
-
     qkt_scaled_HSqSk = qkt_HSqSk / jnp.sqrt(qkt_HSqSk.shape[-1])
     attn_HSqSk = jax.nn.softmax(qkt_scaled_HSqSk, axis=-1)
-
-    # outs_HSDh = einsum(attn_HSqSk, v_HSDh, 'h sq sk, h sk dh -> h sq dh')
     outs_HSDh = attn_HSqSk @ v_HSDh
-
-    # out_SDh = rearrange(outs_HSDh, 'h s dh -> s (h dh)')
-    # out_SDh = jnp.permute_dims(outs_HSDh, (0, 2, 3, 1)).reshape((residual_SDm.shape[0], residual_SDm.shape[1], -1))
-    out_SDh = jnp.permute_dims(outs_HSDh, (1, 2, 0)).reshape((residual_SDm.shape[0], -1))
-
+    out_SDh = jnp.permute_dims(outs_HSDh, (1, 2, 0)).reshape(
+        (residual_SDm.shape[0], -1)
+    )
     return out_SDh
+
 
 def mha_weights(D_model, D_head, n_heads, key):
     init = jax.nn.initializers.xavier_normal()
@@ -49,6 +39,7 @@ def ff(x_SDm: Array, params: tuple[Array, Array, Array, Array]):
     x_SDm = x_SDff @ w2_DffDm + b2_Dm
     return x_SDm
 
+
 def ff_weights(D_model, D_ff, key):
     init1 = jax.nn.initializers.xavier_normal()
     init2 = jax.nn.initializers.xavier_normal()
@@ -62,18 +53,12 @@ def ff_weights(D_model, D_ff, key):
 @jax.jit
 def block(res_SDm: Array, params):
     w_qkv_HDm3Dh, ff_params = params
-
     x = layer_norm(res_SDm)
-
-    # x = jax.vmap(mha, in_axes=(0, None))(x, w_qkv_HDm3Dh=w_qkv_HDm3Dh)
     x = mha(x, w_qkv_HDm3Dh=w_qkv_HDm3Dh)
-
     x = layer_norm(x)
-
-    # x = jax.vmap(ff, in_axes=(0, None))(x, params=ff_params)
     x = ff(x, params=ff_params)
-
     return x
+
 
 def block_weights(D_model, D_head, n_heads, D_ff, key):
     w_qkv_HDm3Dh = mha_weights(D_model, D_head, n_heads, key)
@@ -84,9 +69,9 @@ def block_weights(D_model, D_head, n_heads, D_ff, key):
 @jax.jit
 def blocks(x_SDm: Array, blocks_params):
     for block_params in blocks_params:
-        # x_SDm = jax.vmap(block, in_axes=(0, None))(x_SDm, block_params)
         x_SDm = block(x_SDm, block_params)
     return x_SDm
+
 
 def make_blocks_weights(D_model, D_head, n_heads, D_ff, n_blocks, key):
     return [block_weights(D_model, D_head, n_heads, D_ff, key) for _ in range(n_blocks)]
@@ -96,14 +81,19 @@ def make_blocks_weights(D_model, D_head, n_heads, D_ff, n_blocks, key):
 @jax.jit
 def embed(x_SDi: Array, w_DiDm: Array):
     return x_SDi @ w_DiDm
+
+
 def make_embed_weights(D_vocab, D_model, key):
     init = jax.nn.initializers.xavier_normal()
     w_DiDm = init(key, (D_vocab, D_model))
     return w_DiDm
 
+
 @jax.jit
 def unembed(x_SDm: Array, w_DmDi: Array):
     return x_SDm @ w_DmDi
+
+
 def make_unembed_weights(D_model, D_vocab, key):
     init = jax.nn.initializers.xavier_normal()
     w_DmDi = init(key, (D_model, D_vocab))
@@ -128,22 +118,24 @@ class ModelCfg:
     def __post_init__(self):
         assert self.D_head * self.n_heads == self.D_model
 
+
 @jax.jit
 def model(x_SDi: Array, params):
     w_DiDm, blocks_params, w_DmDi = params
-    # x_SDm = jax.vmap(embed, in_axes=(0, None))(x_SDi, w_DiDm)
     x_SDm = embed(x_SDi, w_DiDm)
-    # x_SDm = jax.vmap(blocks, in_axes=(0, None))(x_SDm, blocks_params)
     x_SDm = blocks(x_SDm, blocks_params)
-    # x_SDi = jax.vmap(unembed, in_axes=(0, None))(x_SDm, w_DmDi)
     x_SDi = unembed(x_SDm, w_DmDi)
     return x_SDi
 
+
 def make_model_weights(cfg: ModelCfg, key):
     w_DiDm = make_embed_weights(cfg.D_vocab, cfg.D_model, key)
-    blocks_params = make_blocks_weights(cfg.D_model, cfg.D_head, cfg.n_heads, cfg.D_ff, cfg.n_blocks, key)
+    blocks_params = make_blocks_weights(
+        cfg.D_model, cfg.D_head, cfg.n_heads, cfg.D_ff, cfg.n_blocks, key
+    )
     w_DmDi = make_unembed_weights(cfg.D_model, cfg.D_vocab, key)
     return w_DiDm, blocks_params, w_DmDi
+
 
 @dataclass
 class TrainCfg:
@@ -153,45 +145,47 @@ class TrainCfg:
     n_epochs: int
     model_cfg: ModelCfg
 
+
 def fake_main():
+    model_cfg = ModelCfg(
+        D_vocab=16,
+        D_model=32,
+        D_head=8,
+        n_heads=4,
+        D_ff=64,
+        n_blocks=8,
+    )
+
     cfg = TrainCfg(
-        lr = 4e-4,
-        n_epochs=1000,
-        batch_size = 2,
-        seq_len = 10,
-        model_cfg = ModelCfg(
-            D_vocab = 16,
-            D_model = 32,
-            D_head = 8,
-            n_heads = 4,
-            D_ff = 64,
-            n_blocks = 8,
-        )
+        lr=4e-4, n_epochs=1000, batch_size=2, seq_len=10, model_cfg=model_cfg
     )
 
     key = random.PRNGKey(0)
 
     fake_train(cfg, key)
 
+
+model_b = jax.vmap(model, in_axes=(0, None))
+
+
 @jax.jit
 def loss_fn(params, x, y):
-    y_pred = jax.vmap(model, in_axes=(0, None))(x, params)
-    # y_pred = model(x, params)
+    y_pred = model_b(x, params)
     return jnp.mean(jnp.square(y_pred - y))
+
 
 @jax.jit
 def update(params, grads, lr):
-  return tree_util.tree_map(lambda w, g: w - lr * g, params, grads)
+    return tree_util.tree_map(lambda w, g: w - lr * g, params, grads)
+
 
 def create_ones_batch(batch_size: int, seq_len: int, d_vocab: int, key: Array):
     key, subkey = random.split(key)
-    # x = random.randint(subkey, (batch_size, seq_len), 0, d_vocab)
     x = random.normal(subkey, (batch_size, seq_len, d_vocab))
     key, subkey = random.split(key)
-    # y = random.randint(subkey, (batch_size, seq_len), 0, d_vocab)
-    # y = random.ones(subkey, (batch_size, seq_len, d_vocab))
     y = jnp.ones((batch_size, seq_len, d_vocab))
     return x, y
+
 
 def fake_train(cfg: TrainCfg, key: Array):
     params = make_model_weights(cfg.model_cfg, key)
@@ -200,35 +194,13 @@ def fake_train(cfg: TrainCfg, key: Array):
 
     for epoch in range(cfg.n_epochs):
         key, subkey = random.split(key)
-        x, y = create_ones_batch(cfg.batch_size, cfg.seq_len, cfg.model_cfg.D_vocab, subkey)
+        x, y = create_ones_batch(
+            cfg.batch_size, cfg.seq_len, cfg.model_cfg.D_vocab, subkey
+        )
         loss, grads = loss_grad(params, x, y)
         params = update(params, grads, cfg.lr)
         print(f"epoch {epoch}, loss {loss}")
 
-# def create_example(seq_len: int, d_vocab: int, key: Array):
-#     key, subkey = random.split(key)
-#     # x = random.randint(subkey, (batch_size, seq_len), 0, d_vocab)
-#     x = random.normal(subkey, (seq_len, d_vocab))
-#     key, subkey = random.split(key)
-#     # y = random.randint(subkey, (batch_size, seq_len), 0, d_vocab)
-#     y = random.normal(subkey, (seq_len, d_vocab))
-#     return x, y, key
 
 if __name__ == "__main__":
     fake_main()
-
-@jax.jit
-def attention(residual: Array, w_qkv_Dm3Dh):
-    '''
-    residual: (batch, seq_len, dim_model)
-    w_qkv_Dm3Dh: (d_model, 3 * d_head)
-    '''
-    qkv = einsum(residual, w_qkv_Dm3Dh, "b s dm, dm dh_x3 -> b s dh_x3")
-    q, k, v = jnp.split(qkv, 3, axis=-1)
-    qkt = einsum(q, k, "b sq dh, b sk dh -> b sq sk") # QK^T
-    qkt_scaled = qkt / jnp.sqrt(qkt.shape[-1])
-    attn = jax.nn.softmax(qkt_scaled, axis=-1)
-    out = einsum(attn, v, 'b s sk, b s dh -> b s dh')
-    return out
-
-#     print(jnp.einsum("batch dim1 dim2, dim2 dim3 -> batch dim1 dim2", jnp.ones((2, 3, 4)), jnp.ones((4, 5))))
