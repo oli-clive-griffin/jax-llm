b0b1c20aa322766655edbf000992154120bb3890
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2024-06-24 16:15:51 -0400

    wip

--- model_raw.py
+++ model_raw.py
@@ -78,25 +78,24 @@ def make_blocks_weights(D_model, D_head, n_heads, D_ff, n_blocks, key):
 
 # there's gotta be a better way to do this with onehot encoding like nn.Embedding
 @jax.jit
-def embed(x_SDi: Array, w_DiDm: Array):
-    return x_SDi @ w_DiDm
-
+def embed(x_S: Array, w_VDm: Array):
+    return jnp.take(w_VDm, x_S, axis=0)
 
 def make_embed_weights(D_vocab, D_model, key):
     init = jax.nn.initializers.xavier_normal()
-    w_DiDm = init(key, (D_vocab, D_model))
-    return w_DiDm
+    w_VDm = init(key, (D_vocab, D_model))
+    return w_VDm
 
 
 @jax.jit
-def unembed(x_SDm: Array, w_DmDi: Array):
-    return x_SDm @ w_DmDi
+def unembed(x_SDm: Array, w_DmV: Array):
+    return x_SDm @ w_DmV
 
 
 def make_unembed_weights(D_model, D_vocab, key):
     init = jax.nn.initializers.xavier_normal()
-    w_DmDi = init(key, (D_model, D_vocab))
-    return w_DmDi
+    w_DmV = init(key, (D_model, D_vocab))
+    return w_DmV
 
 
 def layer_norm(x, epsilon=1e-5):
@@ -119,12 +118,12 @@ class ModelCfg:
 
 
 @jax.jit
-def model(x_SDi: Array, params):
+def model(x_S: Array, params):
     w_DiDm, blocks_params, w_DmDi = params
-    x_SDm = embed(x_SDi, w_DiDm)
+    x_SDm = embed(x_S, w_DiDm)
     x_SDm = blocks(x_SDm, blocks_params)
-    x_SDi = unembed(x_SDm, w_DmDi)
-    return x_SDi
+    x_SV = unembed(x_SDm, w_DmDi)
+    return x_SV
 
 
 def make_model_weights(cfg: ModelCfg, key):
--- train.py
+++ train.py
@@ -4,6 +4,9 @@ from dataclasses import dataclass
 from jax import random, Array
 import jax.numpy as jnp
 from model_raw import ModelCfg, model, make_model_weights
+from functools import partial
+
+jax.config.update("jax_debug_nans", True)
 
 @dataclass
 class TrainCfg:
@@ -17,15 +20,15 @@ class TrainCfg:
 def fake_main():
     model_cfg = ModelCfg(
         D_vocab=16,
-        D_model=32,
-        D_head=8,
+        D_model=128,
+        D_head=32,
         n_heads=4,
         D_ff=64,
-        n_blocks=8,
+        n_blocks=2,
     )
 
     cfg = TrainCfg(
-        lr=4e-4, n_epochs=1000, batch_size=2, seq_len=10, model_cfg=model_cfg
+        lr=4e-4, n_epochs=100_000, batch_size=2, seq_len=10, model_cfg=model_cfg
     )
 
     key = random.PRNGKey(0)
@@ -37,36 +40,42 @@ model_b = jax.vmap(model, in_axes=(0, None))
 
 
 @jax.jit
-def loss_fn(params, x, y):
-    y_pred = model_b(x, params)
-    return jnp.mean(jnp.square(y_pred - y))
+def loss_fn(params, x_BS, y_BSV):
+    y_pred_BSV = model_b(x_BS, params)
+    return cross_entropy(y_pred_BSV, y_BSV)
 
+
+@jax.jit
+def cross_entropy(logits_x_BSV, y_BSV):
+    return -jnp.mean(jnp.sum(y_BSV * jax.nn.log_softmax(logits_x_BSV), axis=-1))
 
 @jax.jit
 def update(params, grads, lr):
     return tree_util.tree_map(lambda w, g: w - lr * g, params, grads)
 
 
-def create_ones_batch(batch_size: int, seq_len: int, d_vocab: int, key: Array):
+def create_id_batch(batch_size: int, seq_len: int, d_vocab: int, key: Array):
     key, subkey = random.split(key)
-    x = random.normal(subkey, (batch_size, seq_len, d_vocab))
-    key, subkey = random.split(key)
-    y = jnp.ones((batch_size, seq_len, d_vocab))
-    return x, y
+    x = random.randint(subkey, (batch_size, seq_len), 0, d_vocab)
+    return x, x.copy()
+
 
 def fake_train(cfg: TrainCfg, key: Array):
     params = make_model_weights(cfg.model_cfg, key)
 
+    # loss_grad = partial(jax.value_and_grad(loss_fn), vocab_size=cfg.model_cfg.D_vocab)
     loss_grad = jax.value_and_grad(loss_fn)
 
     for epoch in range(cfg.n_epochs):
         key, subkey = random.split(key)
-        x, y = create_ones_batch(
+        x, y = create_id_batch(
             cfg.batch_size, cfg.seq_len, cfg.model_cfg.D_vocab, subkey
         )
-        loss, grads = loss_grad(params, x, y)
+        y_BSV = jax.nn.one_hot(y, cfg.model_cfg.D_vocab)
+        loss, grads = loss_grad(params, x, y_BSV)
         params = update(params, grads, cfg.lr)
-        print(f"epoch {epoch}, loss {loss}")
+        if epoch % 100 == 0:
+            print(f"epoch {epoch}, loss {loss}")
 
 
 if __name__ == "__main__":
